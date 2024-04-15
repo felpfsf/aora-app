c9efc163e393a9e3d933115d1b1857e2c62fd714
Felipe Ferreira <feferreira.sf@gmail.com>
2024-04-15 17:42:36 -0300

    ✨ appwrite config and create user function

---
+++ src/lib/appwrite.js
@@ -0,0 +1,71 @@
+import { Account, Avatars, Client, Databases, ID } from "react-native-appwrite";
+
+export const config = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_KEY,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID,
+  videosCollectionId: process.env.EXPO_PUBLIC_APPWRITE_VIDEOS_COLLECTION_ID,
+  bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
+};
+
+// Init your react-native SDK
+const client = new Client();
+
+client
+  .setEndpoint(config.endpoint) // Your Appwrite Endpoint
+  .setProject(config.projectId) // Your project ID
+  .setPlatform(config.platform);
+
+// Your application ID or bundle ID.
+const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+export const createUser = async (username, email, password) => {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = await avatars.getInitials(username, {
+      backgroundColor: "green",
+      color: "white",
+      size: 128,
+    });
+
+    await signIn(email, password);
+
+    const newUser = await databases.createDocument(
+      config.databaseId,
+      config.userCollectionId,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        username,
+        email,
+        avatarUrl,
+      }
+    );
+
+    return newUser;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export async function signIn(email, password) {
+  try {
+    const session = await account.createEmailSession(email, password);
+
+    return session;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
